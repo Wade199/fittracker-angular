655f12ac2d3a0d37c4c25111fcbd40d225b62143
Ibrahima Wade <ibrahima97wade99@gmail.com>
2026-05-08 18:55:08 +0200

    feat: PWA mobile support - manifest, service worker, mobile CSS optimizations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -32,6 +32,7 @@ import { WorkoutDetailComponent } from './features/workouts/workout-detail/worko
 
 // Progress
 import { ProgressTrackerComponent } from './features/progress/progress-tracker/progress-tracker.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -59,7 +60,13 @@ import { ProgressTrackerComponent } from './features/progress/progress-tracker/p
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     // Enregistre l'intercepteur JWT pour toutes les requêtes HTTP
